bc81929d8c74d957fb46c25e86a3b3fd30ee5af6
amprabhleenkaur <amprabhleenkaur@gmail.com>
2021-03-14 11:32:41 -0400

    user profile model

---
+++ Models/UserProfileModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
+
+namespace SecondHandBook.Models
+{
+    public class UserProfileModel
+    {
+        [NotMappedAttribute]
+        public IFormFile UserImage { get; set; }
+
+        [Required]
+        public String FirstName { get; set; }
+
+        [Required]
+        public String LastName { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public long Mobile { get; set; }
+
+        [Required]
+        public String Address { get; set; }
+
+        [Required]
+        public String City { get; set; }
+
+        [Required]
+        public String Province { get; set; }
+
+        [Required]
+        public String Country { get; set;}
+
+        [Required]
+        public String PostalCode { get; set; }
+
+                
+    }
+}
