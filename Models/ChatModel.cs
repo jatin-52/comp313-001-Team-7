2be203376fe68cb7801b1010443cda3b5319bd56
jatin52 <jatin.verma52@outlook.com>
2021-03-27 21:05:30 -0400

    qr code implementation

---
+++ Models/ChatModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecondHandBook.Models
+{
+    public class ChatModel
+    {
+        [Key]
+        public int ID { get; set; }
+
+        // the user who send the message
+        [Required]
+        public String toUserId { get; set; }
+
+        // the user who send the message
+        [Required]
+        public String fromUserId { get; set; }
+
+        [Required]
+        public String message { get; set; }
+
+        
+    }
+}
